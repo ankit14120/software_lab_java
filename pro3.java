a662baf0ce7a1c31e7f7e14e52f69ba42d62284e
Ankit Patil <90186728+Ankit-Patil2000@users.noreply.github.com>
2021-12-20 13:23:21 +0530

    Add files via upload

---
+++ pro3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class pro2 {
+    public static void main(String[] args) { 
+//variables
+    int a =100,b=150 ;
+    
+    int sum=a+b;
+    int diff=a-b;
+    int mul=a*b;
+    int div=a/b;
+
+   // System.out.println("Entered numbers :" +a, "and:" +b);
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(mul);
+    System.out.println(div);
+    }
+}
