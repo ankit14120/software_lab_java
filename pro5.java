137a91e5577269e523cd78ee547bd44a128562da
Ankit Patil <90186728+Ankit-Patil2000@users.noreply.github.com>
2021-12-23 16:12:35 +0530

    Add files via upload

---
+++ pro5.java
@@ -0,0 +1,13 @@
+public class pro5 {
+    public static void main(String[] args){
+        int a=50, b=5, c=100;
+        
+        if(a>b&&a>c)
+        {System.out.println("Number a is largest\n" +a);}
+        else if(b>a&&b>c)
+        {System.out.println("Number b is largest\n" +b);}
+        else
+        {System.out.println("Number c is largest\n" +c);}
+    }
+    
+}
