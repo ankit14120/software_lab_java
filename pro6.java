41cbe441b43aca96b1e02b889b921b8cd28bf9d0
Ankit Patil <90186728+Ankit-Patil2000@users.noreply.github.com>
2021-12-23 16:15:46 +0530

    Add files via upload

---
+++ pro6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pro6 {
+    public static void main(String[] args){
+
+        Scanner pc= new Scanner(System.in);
+        System.out.println("Enter numbers");
+        int a=pc.nextInt(), b=pc.nextInt(), c=pc.nextInt();
+        
+        if(a>b&&a>c)
+        {System.out.println("Number a is largest\n" +a);}
+        else if(b>a&&b>c)
+        {System.out.println("Number b is largest\n" +b);}
+        else
+        {System.out.println("Number c is largest\n" +c);}
+    }
+    
+}
