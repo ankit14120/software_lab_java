f243ae11f898941dab926e4b0b03b52412c6eb7d
Ankit Patil <90186728+Ankit-Patil2000@users.noreply.github.com>
2022-01-12 15:02:58 +0530

    Add files via upload

---
+++ pro8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+public class pro8
+{
+    public static void main(String[]args){
+        Scanner pc =new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        int a= pc.nextInt();
+        System.out.println("enter the value of b:");
+        int 
+    }
+}
